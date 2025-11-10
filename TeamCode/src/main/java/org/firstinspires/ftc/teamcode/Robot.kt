bde55f22bf15e2165c5decf74f33ac425b2b8a63
NHermione <148692336+NHermione@users.noreply.github.com>
2025-11-10 15:47:48 +0100

    Implemented a drivebase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
@@ -1,13 +1,24 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.hardware.DcMotor
-import org.firstinspires.ftc.teamcode.internal.RobotOpModeBase
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.firstinspires.ftc.teamcode.internal.RobotOpMode
 
 
-abstract class Robot() : RobotOpModeBase() {
-    val mtr : DcMotor by hardware()
+class Robot(opmode: RobotOpMode) {
+    val mtr = opmode.hardware<DcMotor>("mtr")
 
-    override fun init() {
-        mtr.mode = DcMotor.RunMode.RUN_USING_ENCODER
+    init {
+        mtr.direction = DcMotorSimple.Direction.REVERSE
     }
+}
+
+fun RobotOpMode.auto() {
+    robot.mtr.power = 1.0
+    telemetry.addData("Elapsed", "$elapsed")
+}
+
+fun RobotOpMode.teleop() {
+    robot.mtr.power = -0.5
+    telemetry.addData("Elapsed", "$runtime")
 }
