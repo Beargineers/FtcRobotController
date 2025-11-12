35931650ad231531b0378f3d5e494ab5eb2f6ad0
Maxim Shafirov <max@jetbrains.com>
2025-11-12 22:30:14 +0100

    Cosmetic changes to AprilTagWebcam.kt to make it more Kotlin idiomatic.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
@@ -4,14 +4,17 @@ import org.firstinspires.ftc.teamcode.internal.RobotOpModeBase
 
 abstract class Robot() : RobotOpModeBase() {
     lateinit var drive: Drivebase
+    lateinit var aprilTags: AprilTagWebcam
 
     override fun init() {
         drive = Drivebase(this)
+        aprilTags = AprilTagWebcam(this)
 
         telemetry.addLine("Ready")
     }
 
     override fun stop() {
         drive.stop()
+        aprilTags.stop()
     }
 }
