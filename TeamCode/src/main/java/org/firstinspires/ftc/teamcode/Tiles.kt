c98452105ad53edd6c887e57335a86ef984cca0b
Maxim Shafirov <max@shafirov.com>
2025-11-14 16:33:42 +0100

    - Introduce `Position2D` and `Pose2D` data classes to represent robot position and orientation on the field. - Split the original `FieldCoordinates.kt` into more focused files:     - `Coordinates.kt`: Defines the new `Position2D` and `Pose2D` classes.     - `Tiles.kt`: Contains functions for calculating tile positions.     - `Field.kt`: Defines field-specific constants like `FIELD_CENTER` and `Spike` locations, now using `Pose2D`. - Update `AprilTagWebcam` to use the new `Pose2D` class instead of the FTC `Position` class for determining robot pose.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tiles.kt
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+
+/*
+Tiles are marked vertically 1 (lowest row) through 6 (highest row)
+and A (leftmost column) through F (rightmost column) horizontally, where A6 holds blue goal and F6 holds red goal.
+Each tile is 26x26 inches
+ */
+
+enum class TileOffset(val yoffset: Int, val xoffset: Int) {
+    CENTER(0, 0),
+
+    TOP_LEFT(-1, -1),
+    TOP_RIGHT(+1, -1),
+
+    BOTTOM_LEFT(-1, +1),
+    BOTTOM_RIGHT(+1, +1),
+
+    LEFT_CENTER(-1, 0),
+    RIGHT_CENTER(+1, 0),
+
+    TOP_CENTER(0, -1),
+    BOTTOM_CENTER(0, +1)
+}
+
+fun tilePosition(tileCode: String, tileOffset: TileOffset = TileOffset.CENTER): Position2D {
+    val (rowCode, columnCode) = tileCode.partition { it.isDigit() }
+
+    val xIn = ('6' - rowCode[0] - 3) * 26 + (tileOffset.xoffset + 1) * 13
+    val yIn = (columnCode[0] - 'A' - 3) * 26 + (tileOffset.yoffset + 1) * 13
+
+    return Position2D(xIn.toDouble(), yIn.toDouble(), DistanceUnit.INCH).toUnit(DISTANCE_UNIT)
+}
