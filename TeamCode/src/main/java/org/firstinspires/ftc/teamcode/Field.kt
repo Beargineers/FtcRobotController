c0806135cd4baa19e30a51651fc50c6a6c8f7825
Maxim Shafirov <max@shafirov.com>
2025-11-20 12:42:08 +0100

    Two example autonomous trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Field.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Field.kt
@@ -5,10 +5,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 val FIELD_CENTER = Position2D(0.0, 0.0)
 
 // TODO: Identify actual launch points
-val SOUTH_RED_LAUNCH_POINT = Pose2D(0.0, 0.0, 0.0)
-val NORTH_RED_LAUNCH_POINT = Pose2D(0.0, 0.0, 0.0)
-val SOUTH_BLUE_LAUNCH_POINT = Pose2D(0.0, 0.0, 0.0)
-val NORTH_BLUE_LAUNCH_POINT = Pose2D(0.0, 0.0, 0.0)
+val SOUTH_RED_LAUNCH_POINT = tilePosition("D1").withHeading(160.0, AngleUnit.DEGREES)
+val NORTH_RED_LAUNCH_POINT = tilePosition("D5", TileOffset.BOTTOM_RIGHT).withHeading(140.0, AngleUnit.DEGREES)
+val SOUTH_BLUE_LAUNCH_POINT = tilePosition("C1").withHeading(-160.0, AngleUnit.DEGREES)
+val NORTH_BLUE_LAUNCH_POINT = tilePosition("C5", TileOffset.BOTTOM_LEFT).withHeading(-140.0, AngleUnit.DEGREES)
 
 enum class Spike(start: String, end: String) {
     LEFT1("B2", "A2"),
