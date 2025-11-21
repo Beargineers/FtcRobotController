ce11804ac9ce7e4320fde34364c0ba141be42084
Maxim Shafirov <max@shafirov.com>
2025-11-21 17:15:19 +0100

    Compensate flywheel speed based on distance to the goal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.kt
@@ -6,11 +6,12 @@ import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.teamcode.internal.Hardware
 
-private const val FEEDER_RUN_TIME: Long = 4500L
-
 @Configurable
 object ShooterConfig {
-    var SHOOTER_P = 0.70
+    var SHOOTER_POWER_ADJUST = 1.0
+    var SHOOTER_DISTANCE_QUOTIENT = 0.00101
+    var SHOOTER_FREE_QUOTIENT = 0.556
+    var SHOOTING_TIME_SECONDS: Double = 4.5
 }
 
 class Shooter(op: OpMode): Hardware(op) {
@@ -21,10 +22,14 @@ class Shooter(op: OpMode): Hardware(op) {
 
     var feederStartedAt: Long = 0
 
+    var flywheelEnabled = false
+    var frozenFlywheelPower: Double? = null
+
     val launchMotors = listOf(fly1, fly2)
 
     init {
         launchMotors.forEach {
+            it.direction = DcMotorSimple.Direction.REVERSE
             it.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
             it.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
         }
@@ -34,32 +39,41 @@ class Shooter(op: OpMode): Hardware(op) {
     }
 
     fun enableFlywheel(on: Boolean) {
-        if (on) {
-            powerFlywheel(ShooterConfig.SHOOTER_P)
-        }
-        else {
-            powerFlywheel(0.0)
-        }
+        flywheelEnabled = on
     }
 
     private fun powerFlywheel(p: Double) {
-        fly1.direction = DcMotorSimple.Direction.REVERSE
-        fly2.direction = DcMotorSimple.Direction.REVERSE
         setMotorPower(fly1, p)
         setMotorPower(fly2, p)
     }
 
     fun launch() {
+        frozenFlywheelPower = recommendedFlywheelPower()
+
         feeder.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
         setMotorPower(feeder, 1.0)
         feederStartedAt = System.currentTimeMillis()
     }
 
+    private fun recommendedFlywheelPower(): Double = flywheelPowerAdjustedToDistance((op as Robot).goalDistanceCM ?: 90.0)
+
     override fun loop() {
-        if (feederStartedAt != 0L && (System.currentTimeMillis() - feederStartedAt) > FEEDER_RUN_TIME) {
+        if (feederStartedAt != 0L && (System.currentTimeMillis() - feederStartedAt) > ShooterConfig.SHOOTING_TIME_SECONDS * 1000) {
             setMotorPower(feeder, 0.0)
             feederStartedAt = 0L
+            frozenFlywheelPower = null
         }
+
+        val nominalPower = when {
+            flywheelEnabled -> frozenFlywheelPower ?: recommendedFlywheelPower()
+            else -> 0.0
+        }
+        powerFlywheel(nominalPower)
+    }
+
+    // According to the experimental data this linear approximation gives coefficient of determination of 0.95. Also, voltage degradation seem to be handled quite well
+    fun flywheelPowerAdjustedToDistance(distanceCm: Double): Double {
+        return (ShooterConfig.SHOOTER_DISTANCE_QUOTIENT *distanceCm+ ShooterConfig.SHOOTER_FREE_QUOTIENT) * ShooterConfig.SHOOTER_POWER_ADJUST
     }
 
     override fun stop() {
