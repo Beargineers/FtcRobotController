9fa728a2015005bc4e69e1f908aef8fc34fa01f8
Maxim Shafirov <max@jetbrains.com>
2025-11-26 12:28:28 +0100

    Tweak for more compact phases DSL

--- TeamCode/src/main/java/org/beargineers/DecodeOpMode.kt
+++ TeamCode/src/main/java/org/beargineers/DecodeOpMode.kt
@@ -2,6 +2,7 @@ package org.beargineers
 
 import org.beargineers.platform.Alliance
 import org.beargineers.platform.PhasedAutonomous
+import org.beargineers.platform.Phases
 import org.beargineers.platform.RobotOpMode
 
 open class DecodeOpMode(alliance: Alliance) : RobotOpMode<DecodeRobot>(alliance) {
@@ -10,7 +11,7 @@ open class DecodeOpMode(alliance: Alliance) : RobotOpMode<DecodeRobot>(alliance)
     }
 }
 
-abstract class DecodeAutonomous(alliance: Alliance) : PhasedAutonomous<DecodeRobot>(alliance) {
+abstract class DecodeAutonomous(alliance: Alliance, phases: Phases<DecodeRobot>) : PhasedAutonomous<DecodeRobot>(alliance, phases) {
     override fun createRobot(opMode: RobotOpMode<DecodeRobot>): DecodeRobot {
         return DecodeRobot(this)
     }
