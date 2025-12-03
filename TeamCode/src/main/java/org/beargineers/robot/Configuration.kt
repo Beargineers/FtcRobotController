1e2138587734168dfa1b4323e8f78824d401aedc
Maxim Shafirov <max@shafirov.com>
2025-12-03 15:31:24 +0100

    Camera position configuration is unique for the particular robot

---
+++ TeamCode/src/main/java/org/beargineers/robot/Configuration.kt
@@ -0,0 +1,13 @@
+package org.beargineers.robot
+
+import com.bylazar.configurables.annotations.Configurable
+
+@Configurable
+object CameraPosition {
+    var forward = 20.0
+    var up = 36.0
+    var right = 0.0
+    var pitch = -90.0
+    var yaw = 0.0
+    var roll = 0.0
+}
