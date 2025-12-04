67788f20f2ce23c9de97e6c9ac04e5f242ae5f97
Maxim Shafirov <max@jetbrains.com>
2025-12-04 13:50:29 +0100

    Let's keep all robot-related configuration in one file

--- TeamCode/src/main/java/org/beargineers/robot/DecodeRobot.kt
+++ TeamCode/src/main/java/org/beargineers/robot/DecodeRobot.kt
@@ -1,7 +1,5 @@
 package org.beargineers.robot
 
-import com.bylazar.configurables.annotations.Configurable
-import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.beargineers.platform.Alliance
 import org.beargineers.platform.AprilTagWebcam
 import org.beargineers.platform.AutonomousDriveConfig
@@ -9,50 +7,7 @@ import org.beargineers.platform.BaseRobot
 import org.beargineers.platform.KalmanFilter
 import org.beargineers.platform.MecanumDrive
 import org.beargineers.platform.RobotOpMode
-import org.beargineers.platform.WheelsConfig
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection
-
-@Configurable
-object WheelCorrections {
-    var LF: Double = -1.0 // Negative value means this motor's encoder reports negative changes when positive power is applied
-    var RF: Double = -0.775
-    var LB: Double = 1.0
-    var RB: Double = 0.925
-
-    var FORWARD_CM_PER_TICK: Double = 0.05752541667
-    var STRAFE_CM_PER_TICK: Double = 0.069 // 0.07096614583 // 0.049 //
-
-    fun asConfig(): WheelsConfig = WheelsConfig(
-        lf_direction = DcMotorSimple.Direction.REVERSE,
-        lb_direction = DcMotorSimple.Direction.REVERSE,
-        rf_direction = DcMotorSimple.Direction.FORWARD,
-        rb_direction = DcMotorSimple.Direction.FORWARD,
-
-        lf_encoder_direction = DcMotorSimple.Direction.REVERSE,
-        rf_encoder_direction = DcMotorSimple.Direction.REVERSE,
-        lb_encoder_direction = DcMotorSimple.Direction.FORWARD,
-        rb_encoder_direction = DcMotorSimple.Direction.FORWARD,
-
-        lf_correction = LF,
-        rf_correction = RF,
-        lb_correction = LB,
-        rb_correction = RB,
-        cm_per_tick_forward = FORWARD_CM_PER_TICK,
-        cm_per_tick_strafe = STRAFE_CM_PER_TICK
-    )
-}
-
-@Configurable
-object AutonomousConfig {
-    var MINIMAL_WHEEL_POWER: Double = 0.12
-
-    // Max default auto speed. Pass maxSpeed parameter to driveTo to overrun the default
-    var MAX_SPEED = 0.7
-
-
-    var kP_position = 0.035
-    var kP_heading = 0.01
-}
 
 class DecodeRobot(opMode: RobotOpMode<DecodeRobot>) : BaseRobot(opMode) {
     override val drive = MecanumDrive(this, WheelCorrections.asConfig())
