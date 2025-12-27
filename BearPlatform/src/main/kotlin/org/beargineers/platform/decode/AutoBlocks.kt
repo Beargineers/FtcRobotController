f69a2c732b7c3d5ab9cf6b406a97b612beb45b26
Maxim Shafirov <max@shafirov.com>
2025-12-27 19:04:05 +0100

    openGate in auto

--- BearPlatform/src/main/kotlin/org/beargineers/platform/decode/AutoBlocks.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/decode/AutoBlocks.kt
@@ -3,38 +3,48 @@ package org.beargineers.platform.decode
 import com.qualcomm.robotcore.util.ElapsedTime
 import org.beargineers.platform.Alliance
 import org.beargineers.platform.AutonomousPhase
+import org.beargineers.platform.BLUE_OPEN_GATE
+import org.beargineers.platform.BLUE_OPEN_GATE_APPROACH
 import org.beargineers.platform.PhaseBuilder
 import org.beargineers.platform.PhaseDsl
 import org.beargineers.platform.PhasedAutonomous
 import org.beargineers.platform.Position
+import org.beargineers.platform.RED_OPEN_GATE
+import org.beargineers.platform.RED_OPEN_GATE_APPROACH
 import org.beargineers.platform.action
 import org.beargineers.platform.assumeRobotPosition
 import org.beargineers.platform.driveTo
-import org.beargineers.platform.followPath
 import org.beargineers.platform.tilePosition
+import org.beargineers.platform.wait
+import kotlin.time.Duration.Companion.seconds
 
 @PhaseDsl
-fun PhaseBuilder<DecodeRobot>.scoopSpike(spike: Spike) {
+private fun PhaseBuilder<DecodeRobot>.scoopSpike(spike: Spike) {
     seq("Scoop spike ${spike.name}") {
         driveTo(spike.startPose)
-        driveTo(spike.endPose, maxSpeed = 0.15) // Drive in slowly, so we can carefully scoop the artifacts
-//        driveTo(spike.startPose) // Drive out carefully so we don't disturb other artifacts
+        driveTo(spike.endPose) // Drive in slowly, so we can carefully scoop the artifacts
+     //   driveTo(spike.startPose) // Drive out carefully so we don't disturb other artifacts
     }
 }
 
 @PhaseDsl
-fun PhaseBuilder<DecodeRobot>.scoopAndShoot(spike: Spike, launchPose: Position) {
+private fun PhaseBuilder<DecodeRobot>.scoopAndShoot(spike: Spike, launchPose: Position) {
     seq("Scoop and shoot ${spike.name}") {
         scoopSpike(spike)
-        action {
-            enableFlywheel(true)
-        }
-        followPath(listOf(spike.startPose, launchPose))
-        action {
-            launch()
-        }
-        waitForShootingCompletion()
+        shootAt(launchPose)
     }
+}
+
+private fun PhaseBuilder<DecodeRobot>.shootAt(launchPose: Position) {
+    action {
+        enableFlywheel(true)
+    }
+    //  followPath(listOf(spike.startPose, spike.endPose, spike.startPose, launchPose))
+    driveTo(launchPose)
+    action {
+        launch()
+    }
+    waitForShootingCompletion()
 }
 
 open class DecodeAutoStrategy(alliance: Alliance, val positions: String, vararg spikes: Spike) :
@@ -58,9 +68,19 @@ private fun PhaseBuilder<DecodeRobot>.autoStrategy(startingPoint: Position,
 ) {
     assumeRobotPosition(startingPoint)
     shootInitialLoad(launchPoint)
-    for (spike in spikes) {
-        scoopAndShoot(spike, launchPoint)
-    }
+    scoopSpike(spikes[0])
+    openRamp()
+    shootAt(launchPoint)
+    scoopAndShoot(spikes[1], launchPoint)
+    scoopAndShoot(spikes[2], launchPoint)
+}
+
+private fun PhaseBuilder<DecodeRobot>.openRamp() {
+    val red = opMode.alliance == Alliance.RED
+
+    driveTo(if (red) RED_OPEN_GATE_APPROACH else BLUE_OPEN_GATE_APPROACH)
+    driveTo(if (red) RED_OPEN_GATE else BLUE_OPEN_GATE, 0.8)
+    wait(1.seconds)
 }
 
 @PhaseDsl
