9b311dc9a7ff7563f1f21a207188c5891ac4f79f
YakovShafirov <yakov.shafirov@gmail.com>
2025-12-27 22:13:30 +0100

    Implements automatic parking in teleop,
    Disables lookAtGoal when driver turns manually

--- Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
+++ Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
@@ -1,14 +1,20 @@
 package org.beargineers.straffer
 
 import org.beargineers.platform.AbsoluteLocalizer
+import org.beargineers.platform.Alliance
+import org.beargineers.platform.BLUE_PARK
 import org.beargineers.platform.BaseRobot
 import org.beargineers.platform.LimelightCam
+import org.beargineers.platform.Location
 import org.beargineers.platform.MecanumDrive
 import org.beargineers.platform.PinpointLocalizer
+import org.beargineers.platform.RED_PARK
 import org.beargineers.platform.RelativeLocalizer
 import org.beargineers.platform.RobotOpMode
+import org.beargineers.platform.abs
 import org.beargineers.platform.decode.DecodeRobot
 import org.beargineers.platform.decode.IntakeMode
+import org.beargineers.platform.degrees
 
 class StrafferRobot(op: RobotOpMode<DecodeRobot>) : BaseRobot(op), DecodeRobot {
     override val drive = MecanumDrive(this)
@@ -31,7 +37,12 @@ class StrafferRobot(op: RobotOpMode<DecodeRobot>) : BaseRobot(op), DecodeRobot {
     }
 
     override fun park() {
-        TODO("Not yet implemented")
+        val parkCoords: Location =
+            (if (opMode.alliance == Alliance.BLUE) BLUE_PARK else RED_PARK)
+        val heading = currentPosition.heading
+        val squareAngles = listOf(-180.degrees, -90.degrees, 0.degrees, 90.degrees, 180.degrees)
+        val parkHeading = squareAngles.minBy { abs(it - heading) }
+        driveToTarget(parkCoords.withHeading(parkHeading), 1.0)
     }
 
     override fun isShooting(): Boolean {
