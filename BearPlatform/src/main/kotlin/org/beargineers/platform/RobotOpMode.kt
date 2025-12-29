fedca9bfc6da4fb6d120c089dd25a88563cf0e51
Maxim Shafirov <max@shafirov.com>
2025-12-29 16:22:26 +0100

    Make it possible to call "auto" functions in teleop mode (for going base, ramp, or shooting zone)

--- BearPlatform/src/main/kotlin/org/beargineers/platform/RobotOpMode.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/RobotOpMode.kt
@@ -3,10 +3,11 @@ package org.beargineers.platform
 import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.hardware.lynx.LynxModule.BulkCachingMode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.util.ElapsedTime
 
 
-abstract class RobotOpMode<out T : Robot>(val alliance: Alliance) : OpMode() {
+abstract class RobotOpMode<T : Robot>(val alliance: Alliance) : OpMode() {
     private val allButtons = mutableListOf<Button>()
 
     val robot by lazy { RobotFactory.newRobot(this) }
@@ -16,6 +17,10 @@ abstract class RobotOpMode<out T : Robot>(val alliance: Alliance) : OpMode() {
 
     private val loopsTimer = ElapsedTime()
     private var loopsCount = 0
+
+
+    private var auto: AutonomousPhase<T>? = null
+    private val autoTimer = ElapsedTime()
 
 
     open fun bearInit() {}
@@ -59,6 +64,7 @@ abstract class RobotOpMode<out T : Robot>(val alliance: Alliance) : OpMode() {
 
         telemetry.addData("FPS", "%.1f", FPS)
 
+        loopsCount++
         if (loopsTimer.seconds() > 1) {
             FPS = loopsCount / loopsTimer.seconds()
             loopsCount = 0
@@ -68,8 +74,23 @@ abstract class RobotOpMode<out T : Robot>(val alliance: Alliance) : OpMode() {
         robot.loop()
         allButtons.forEach { it.update() }
 
+
+        if (controlsAreTouched()) {
+            cancelAuto()
+        }
+
+        if (auto != null) {
+            with(auto!!) {
+                if (robot.loopPhase(autoTimer)) {
+                    telemetry.addLine("Auto: ${name()}")
+                    return
+                } else {
+                    cancelAuto()
+                }
+            }
+        }
+
         bearLoop()
-        loopsCount++
     }
 
     fun button(test: () -> Boolean, callback: () -> Unit) {
@@ -78,5 +99,39 @@ abstract class RobotOpMode<out T : Robot>(val alliance: Alliance) : OpMode() {
 
     fun toggleButton(name: String, test: () -> Boolean, callback: (Boolean) -> Unit) {
         allButtons += ToggleButton(name, telemetry, test, callback)
+    }
+
+    fun auto(b: Phases<T>) {
+        val builder = PhaseBuilder<T>(this)
+        builder.b()
+        val phases = builder.build()
+        auto = when (phases.size) {
+            1 -> phases.first()
+            0 -> return
+            else -> SequentialPhase("Auto", phases)
+        }.also {
+            with(it) {
+                robot.initPhase()
+            }
+        }
+        autoTimer.reset()
+    }
+
+    fun cancelAuto() {
+        auto = null
+    }
+
+    fun controlsAreTouched(): Boolean {
+        return gamepad1.controlsAreTouched() || gamepad2.controlsAreTouched()
+    }
+
+    fun Float.touched(): Boolean {
+        return this != 0f
+    }
+
+    fun Gamepad.controlsAreTouched(): Boolean {
+        return left_trigger.touched() || right_trigger.touched() ||
+                left_stick_x.touched() || left_stick_y.touched() ||
+                right_stick_x.touched() || right_stick_y.touched()
     }
 }
