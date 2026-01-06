7d11d571e6f98faef958fdd63829026dc8cf8574
Maxim Shafirov <max@shafirov.com>
2026-01-06 12:59:01 +0100

    Make camera's acceptable results deviations configurable

--- BearPlatform/src/main/kotlin/org/beargineers/platform/Camera.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/Camera.kt
@@ -8,4 +8,7 @@ abstract class Camera(robot: BaseRobot) : Hardware(robot), AbsoluteLocalizer {
     val CameraPosition_pitch by robot.config(0.0)
     val CameraPosition_yaw by robot.config(0.0)
     val CameraPosition_roll by robot.config(0.0)
+
+    val Camera_positionTolerance by robot.config(1.0)
+    val Camera_headingTolerance by robot.config(1.0)
 }
