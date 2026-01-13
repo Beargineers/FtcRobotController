404f29e2a90aa4c075fe080a1348ea3b2f6770e4
Maxim Shafirov <max@jetbrains.com>
2026-01-13 12:58:33 +0100

    Distance sensor example

--- Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
+++ Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
@@ -1,8 +1,10 @@
 package org.beargineers.straffer
 
+import com.qualcomm.robotcore.hardware.DistanceSensor
 import org.beargineers.platform.AbsoluteLocalizer
 import org.beargineers.platform.Angle
 import org.beargineers.platform.BaseRobot
+import org.beargineers.platform.Hardware
 import org.beargineers.platform.LimelightCam
 import org.beargineers.platform.MecanumDrive
 import org.beargineers.platform.PinpointLocalizer
@@ -11,12 +13,24 @@ import org.beargineers.platform.RobotOpMode
 import org.beargineers.platform.decode.DecodeRobot
 import org.beargineers.platform.decode.IntakeMode
 import org.beargineers.platform.degrees
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+
+class Sensors(robot: BaseRobot) : Hardware(robot) {
+    val distance by hardware<DistanceSensor>()
+
+    override fun loop() {
+        super.loop()
+        telemetry.addData("DISTANCE", distance.getDistance(DistanceUnit.CM))
+    }
+}
 
 class StrafferRobot(op: RobotOpMode<DecodeRobot>) : BaseRobot(op), DecodeRobot {
     override val drive = MecanumDrive(this)
 
     override val absoluteLocalizer: AbsoluteLocalizer = LimelightCam(this)
     override val relativeLocalizer: RelativeLocalizer = PinpointLocalizer(this)
+
+    val sensors = Sensors(this)
 
     override val configResource: Int = R.raw.config
     override val intakeMode: IntakeMode get() = IntakeMode.OFF
