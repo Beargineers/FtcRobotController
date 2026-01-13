576f9253254f4e5d3c36b0da141c93294b0fb8cc
YakovShafirov <yakov.shafirov@gmail.com>
2026-01-13 20:22:36 +0100

    Counts the balls, tuned(not final)

--- Beta/src/main/java/org/beargineers/beta/Intake.kt
+++ Beta/src/main/java/org/beargineers/beta/Intake.kt
@@ -2,14 +2,25 @@ package org.beargineers.beta
 
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.DistanceSensor
 import org.beargineers.platform.BaseRobot
 import org.beargineers.platform.Hardware
+import org.beargineers.platform.config
 import org.beargineers.platform.decode.IntakeMode
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 
 class Intake(robot: BaseRobot): Hardware(robot) {
     private val intake: DcMotor by hardware("intake")
+    private val ballDetector: DistanceSensor by hardware("ball")
+
+    val sensorDistanceTreshold by robot.config(10.0)
 
     var mode: IntakeMode = IntakeMode.OFF
+
+    var artifacts: Int = 0
+    var isHole: Int = 0
+    var sensorSee: Boolean = false
+    var ballInIntake: Boolean = false
 
     override fun init() {
         intake.direction = DcMotorSimple.Direction.REVERSE
@@ -29,6 +40,35 @@ class Intake(robot: BaseRobot): Hardware(robot) {
                 setMotorPower(intake, -1.0)
             }
         }
+
+        sensorSee = ballDetector.getDistance(DistanceUnit.CM) < sensorDistanceTreshold
+
+        when {
+            sensorSee && !ballInIntake -> {
+                ballInIntake = true
+                artifacts += 1
+                isHole = 5
+                if (artifacts == 3) {
+                    mode = IntakeMode.OFF
+                }
+            }
+            isHole > 0 && !sensorSee -> {
+                isHole -= 1
+            }
+            !sensorSee -> {
+                ballInIntake = false
+            }
+            else -> {
+                isHole = 5
+            }
+        }
+
+        telemetry.addData("Artifacts", artifacts)
+    }
+
+    fun onShoot(){
+        artifacts = 0
+        mode=IntakeMode.ON
     }
 
     override fun stop() {
