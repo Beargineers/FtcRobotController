03e2b1007fea32a1dddaec3a7c14d08df7c771bb
Maxim Shafirov <max@jetbrains.com>
2026-01-16 20:45:06 +0100

    Disable non-FPV mode for now

--- BearPlatform/src/main/kotlin/org/beargineers/platform/decode/Teleop.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/decode/Teleop.kt
@@ -17,7 +17,7 @@ open class Driving(alliance: Alliance) : RobotOpMode<DecodeRobot>(alliance) {
     val ROTATIONAL_GAIN by robot.config(20)
 
     val slowCoeff by robot.config(0.4)
-    private var fpvDrive = true
+    private var fpvDrive = false
     private var lookAtGoal = false
     private var lookAtGoalBtnClickedAt = 0L
 
@@ -34,9 +34,11 @@ open class Driving(alliance: Alliance) : RobotOpMode<DecodeRobot>(alliance) {
             robot.intakeMode(IntakeMode.REVERSE)
         }
 
+/*
         toggleButton("FPV Drive", gamepad1::b) {
             fpvDrive = it
         }
+*/
 
         toggleButton("Shooter", gamepad1::a){
             robot.enableFlywheel(it)
