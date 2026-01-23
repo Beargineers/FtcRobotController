43940030b4297dd636fd725b556607ef91326783
Maxim Shafirov <max@shafirov.com>
2026-01-23 21:48:33 +0100

    Only trust camera if within 15 degree range

--- BearPlatform/src/main/kotlin/org/beargineers/platform/LimelightCam.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/LimelightCam.kt
@@ -9,6 +9,10 @@ import java.util.concurrent.TimeUnit
 private val executor = Executors.newSingleThreadScheduledExecutor()
 
 class LimelightCam(robot: BaseRobot): Camera(robot) {
+    val Camera_positionTolerance by config(1.0)
+    val Camera_angleRange by config(15)
+    val Camera_headingTolerance by config(1.0)
+
     private val normalizer: PositionNormalDistribution = PositionNormalDistribution(5)
 
     private var lastUpdated = 0.0
@@ -45,13 +49,29 @@ class LimelightCam(robot: BaseRobot): Camera(robot) {
             return null
         }
 
-        return normalizer.result(Camera_positionTolerance.cm, Camera_headingTolerance.degrees)
+        val result = normalizer.result(Camera_positionTolerance.cm, Camera_headingTolerance.degrees)
+        return result?.takeIf { isGoodResult(it) }
+    }
+
+    private fun isGoodResult(result: Position): Boolean {
+        return isGoodResultFor(BLUE_GOAL, result) || isGoodResultFor(RED_GOAL, result)
+    }
+
+    private fun isGoodResultFor(result: Position, goal: Position): Boolean {
+        val headingToGoal = headingFromTo(result.location(), goal.location())
+        if (abs(headingToGoal - result.heading) > Camera_angleRange.degrees) return false
+        if (abs(goal.heading - result.heading) > Camera_angleRange.degrees) return false
+        return true
+    }
+
+    fun headingFromTo(from: Location, to: Location): Angle {
+        return atan2(from.y - to.y, from.x - to.x)
     }
 
     private fun position(latestResult: LLResult): Position? {
-        val position = latestResult.botpose?.robotPose()?.rotate(CameraPosition_yaw.degrees) ?: return null
-        val cameraOffset = Location(-CameraPosition_right.cm, -CameraPosition_forward.cm)
-
-        return cameraOffset.toAbsolute(position).withHeading(position.heading)
+        return latestResult.botpose?.robotPose()
     }
+
+    private val BLUE_GOAL = Position(-58.3727.inch, -55.6425.inch, -128.degrees)
+    private val RED_GOAL = Position(-58.3727.inch, 55.6425.inch, 128.degrees)
 }
