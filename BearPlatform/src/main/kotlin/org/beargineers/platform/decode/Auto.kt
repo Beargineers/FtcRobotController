92f98c4de0945105f49d77e2061aac4092d03fd9
YakovShafirov <yakov.shafirov@gmail.com>
2026-01-23 23:20:35 +0100

    Transfers shooting and starting points to config, and refines the far autonomous

--- BearPlatform/src/main/kotlin/org/beargineers/platform/decode/Auto.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/decode/Auto.kt
@@ -1,40 +1,26 @@
 package org.beargineers.platform.decode
 
-import com.bylazar.configurables.annotations.Configurable
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.beargineers.platform.Alliance
 
-@Configurable
-// Starting,Shooting positions and angles
-object AutonomousPrograms {
-    var RedSouth  = "D1:+180,D1:+160"
-    var BlueSouth = "C1:+180,C1:-160"
-    var RedNorth  = "F6BL:+135,D4:+135"
-    var BlueNorth = "A6BR:-135,C4:-135"
-}
-
 @Autonomous
 class RedSouth : DecodeAutoStrategy(
-    Alliance.RED,
-    AutonomousPrograms.RedSouth
+    Alliance.RED, ShootingZones.BACK
 )
 
 @Autonomous
 class RedNorth : DecodeAutoStrategy(
-    Alliance.RED,
-    AutonomousPrograms.RedNorth
+    Alliance.RED, ShootingZones.FRONT
 )
 
 @Autonomous
 class BlueSouth : DecodeAutoStrategy(
-    Alliance.BLUE,
-    AutonomousPrograms.BlueSouth
+    Alliance.BLUE, ShootingZones.BACK
 )
 
 @Autonomous
 class BlueNorth : DecodeAutoStrategy(
-    Alliance.BLUE,
-    AutonomousPrograms.BlueNorth
+    Alliance.BLUE, ShootingZones.FRONT
 )
 
 
