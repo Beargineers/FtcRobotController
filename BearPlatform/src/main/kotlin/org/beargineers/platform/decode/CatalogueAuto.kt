ff9ec5b5eae0b5cecf7cfd40d0ac2319b410ab23
Maxim Shafirov <max@jetbrains.com>
2026-02-04 12:22:42 +0100

    Extract Selector to a separate class so it can be re-used in multiple menu-like opmodes

---
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/decode/CatalogueAuto.kt
@@ -0,0 +1,65 @@
+package org.beargineers.platform.decode
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.beargineers.platform.Alliance
+import org.beargineers.platform.Selector
+import org.beargineers.platform.Selector.Choice
+import org.beargineers.platform.Selector.Option
+import org.beargineers.platform.config
+
+@Autonomous
+class CatalogueAuto: ProgrammedAuto() {
+    val ProgramCatalogue by config("F123")
+    override lateinit var alliance: Alliance
+    override lateinit var program: String
+
+    private fun buildChoices(): Choice {
+        val programOptions = ProgramCatalogue.split(',').map {
+            Option(it.trim(), null) {
+                program = it.trim()
+            }
+        }
+        val frontPrograms = Choice("Program", programOptions.filter { it.name.startsWith('F') })
+        val backPrograms = Choice("Program", programOptions.filter { it.name.startsWith('B') })
+
+        val startingPointChoice = Choice("Starting Point", listOf(
+            Option("Front", frontPrograms) {},
+            Option("Back", backPrograms) {}
+        ))
+
+        val allianceChoice = Choice("Alliance", listOf(
+            Option("Red", startingPointChoice) {
+                alliance = Alliance.RED
+            },
+            Option("Blue", startingPointChoice) {
+                alliance = Alliance.BLUE
+            }
+        ))
+
+        return allianceChoice
+    }
+
+    val selector by lazy {
+        Selector(buildChoices()) {
+            for ((choice, option) in it) {
+                option.f()
+            }
+        }
+    }
+
+    override fun bearInit() {
+        with(selector) {
+            buttons()
+        }
+    }
+
+    override fun init_loop() {
+        val active = with(selector) {
+            update()
+        }
+
+        if (!active) {
+            telemetry.addLine("Program '$program' selected. Ready to START")
+        }
+    }
+}
