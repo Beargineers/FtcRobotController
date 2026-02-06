b2c7d411af0d04fd9d1c50a8ab9d67c5ae994f21
Maxim Shafirov <max@shafirov.com>
2026-02-06 14:48:53 +0100

    Remove unplugged sensors from the testbed. Makes FPS higher

--- Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
+++ Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
@@ -1,10 +1,8 @@
 package org.beargineers.straffer
 
-import com.qualcomm.robotcore.hardware.DistanceSensor
 import org.beargineers.platform.Angle
 import org.beargineers.platform.BaseRobot
 import org.beargineers.platform.FusionLocalizer
-import org.beargineers.platform.Hardware
 import org.beargineers.platform.LimelightCam
 import org.beargineers.platform.Localizer
 import org.beargineers.platform.MecanumDrive
@@ -13,23 +11,12 @@ import org.beargineers.platform.RobotOpMode
 import org.beargineers.platform.decode.DecodeRobot
 import org.beargineers.platform.decode.IntakeMode
 import org.beargineers.platform.degrees
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
-
-class Sensors(robot: BaseRobot) : Hardware(robot) {
-    val distance by hardware<DistanceSensor>()
-
-    override fun loop() {
-        super.loop()
-        telemetry.addData("DISTANCE", distance.getDistance(DistanceUnit.CM))
-    }
-}
 
 class StrafferRobot(op: RobotOpMode<DecodeRobot>) : BaseRobot(op), DecodeRobot {
     override val drive = MecanumDrive(this)
 
     override val localizer: Localizer = FusionLocalizer(telemetry, LimelightCam(this), PinpointLocalizer(this))
 
-    val sensors = Sensors(this)
     override fun adjustShooting(distance: Double, angle: Double) {
 
     }
