59b2df065d6096d6c22650bb7668502e0fd1a471
Maxim Shafirov <max@jetbrains.com>
2026-02-23 11:40:06 +0100

    Testbed for webcam based artifact detection

--- Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
+++ Strafer/src/main/java/org/beargineers/straffer/StrafferRobot.kt
@@ -1,21 +1,29 @@
 package org.beargineers.straffer
 
+import org.beargineers.platform.AbsoluteLocalizer
 import org.beargineers.platform.Angle
 import org.beargineers.platform.BaseRobot
 import org.beargineers.platform.FusionLocalizer
-import org.beargineers.platform.LimelightCam
 import org.beargineers.platform.Localizer
 import org.beargineers.platform.MecanumDrive
 import org.beargineers.platform.PinpointLocalizer
+import org.beargineers.platform.Position
 import org.beargineers.platform.RobotOpMode
 import org.beargineers.platform.decode.DecodeRobot
 import org.beargineers.platform.decode.IntakeMode
 import org.beargineers.platform.degrees
 
+object NoVision : AbsoluteLocalizer {
+    override fun getRobotPose(): Position? {
+        return null
+    }
+}
+
 class StrafferRobot(op: RobotOpMode<DecodeRobot>) : BaseRobot(op), DecodeRobot {
     override val drive = MecanumDrive(this)
+    val vision = ArtifactsVision(this)
 
-    override val localizer: Localizer = FusionLocalizer(telemetry, LimelightCam(this), PinpointLocalizer(this))
+    override val localizer: Localizer = FusionLocalizer(telemetry, NoVision, PinpointLocalizer(this))
 
     override fun adjustShooting(distance: Double, angle: Double) {
 
