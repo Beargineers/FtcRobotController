3fc9bd28e6ca1faf9b2642cf65f22cd5e5915fd0
Maxim Shafirov <max@jetbrains.com>
2026-03-11 12:47:40 +0100

    Initial. Code copied from road-runner-quickstart. Not connected to the main code at all yet

---
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/rr/tuning/ManualFeedbackTuner.java
@@ -0,0 +1,26 @@
+package org.beargineers.platform.rr.tuning;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.beargineers.platform.rr.MecanumDrive;
+
+public final class ManualFeedbackTuner extends LinearOpMode {
+    public static double DISTANCE = 64;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(0, 0, 0))
+                        .lineToX(DISTANCE)
+                        .lineToX(0)
+                        .build());
+        }
+    }
+}
