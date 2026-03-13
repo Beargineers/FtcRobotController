7d44d27f5ab4afe4d9d8db577c707d571fb00d12
Maxim Shafirov <max@shafirov.com>
2026-03-13 22:46:11 +0100

    First run

--- BearPlatform/src/main/kotlin/org/beargineers/platform/rr/tuning/ManualFeedbackTuner.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/rr/tuning/ManualFeedbackTuner.kt
@@ -3,24 +3,28 @@ package org.beargineers.platform.rr.tuning
 import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.ftc.runBlocking
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import org.beargineers.platform.Alliance
-import org.beargineers.platform.BaseRobot
-import org.beargineers.platform.Robot
-import org.beargineers.platform.RobotOpMode
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.beargineers.platform.rr.MecanumDrive
+import org.beargineers.platform.rr.PinpointLocalizer
+import org.beargineers.platform.rr.tuning.TuningOpModes.SimplePinpointLocalizer
 
 @Autonomous(group = "Tune")
-class ManualFeedbackTuner : RobotOpMode<Robot>() {
+class ManualFeedbackTuner : LinearOpMode() {
     val DISTANCE: Double = 64.0
-    override val alliance: Alliance get() = Alliance.RED
-    val drive = MecanumDrive(robot as BaseRobot)
 
-    override fun bearLoop() {
-        runBlocking(
-            drive.actionBuilder(Pose2d(0.0, 0.0, 0.0))
-                .lineToX(DISTANCE)
-                .lineToX(0.0)
-                .build()
-        )
+    override fun runOpMode() {
+        val pl = PinpointLocalizer(hardwareMap, Pose2d(0.0, 0.0, 0.0))
+        val drive = MecanumDrive(hardwareMap,  SimplePinpointLocalizer(pl))
+
+        waitForStart()
+
+        while (opModeIsActive()) {
+            runBlocking(
+                drive.actionBuilder(Pose2d(0.0, 0.0, 0.0))
+                    .lineToX(DISTANCE)
+                    .lineToX(0.0)
+                    .build()
+            )
+        }
     }
 }
