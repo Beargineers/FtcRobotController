eca2d2caa9ac60dabce1db7c3a27f5bfd9e86d48
Maxim Shafirov <max@jetbrains.com>
2026-03-13 14:11:46 +0100

    Adopt new MecanumDrive and Localizer API in RR's tuning opmodes

--- BearPlatform/src/main/kotlin/org/beargineers/platform/rr/tuning/SplineTest.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/rr/tuning/SplineTest.kt
@@ -1,24 +1,24 @@
-package org.beargineers.platform.rr.tuning;
+package org.beargineers.platform.rr.tuning
 
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.roadrunner.Pose2d
+import com.acmerobotics.roadrunner.Vector2d
+import com.acmerobotics.roadrunner.ftc.runBlocking
+import org.beargineers.platform.Alliance
+import org.beargineers.platform.BaseRobot
+import org.beargineers.platform.Robot
+import org.beargineers.platform.RobotOpMode
+import org.beargineers.platform.rr.MecanumDrive
 
-import org.beargineers.platform.rr.MecanumDrive;
-
-public final class SplineTest extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(0, 0, 0);
-        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
-
-        waitForStart();
-
-        Actions.runBlocking(
+class SplineTest : RobotOpMode<Robot>() {
+    override val alliance: Alliance get() = Alliance.RED
+    val drive = MecanumDrive(robot as BaseRobot)
+    override fun bearLoop() {
+        val beginPose = Pose2d(0.0, 0.0, 0.0)
+        runBlocking(
             drive.actionBuilder(beginPose)
-                    .splineTo(new Vector2d(30, 30), Math.PI / 2)
-                    .splineTo(new Vector2d(0, 60), Math.PI)
-                    .build());
+                .splineTo(Vector2d(30.0, 30.0), Math.PI / 2)
+                .splineTo(Vector2d(0.0, 60.0), Math.PI)
+                .build()
+        )
     }
 }
