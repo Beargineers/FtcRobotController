e22913b5346e4d13194663115fa9452cf66e030b
Maxim Shafirov <max@shafirov.com>
2026-03-30 16:58:38 +0200

    Detect how many balls do we have in an intake by measuring motor's current draw. Doesn't work completely accurate because there's high draw spike (over 3 threshold) in the moment we're intaking a ball from a side

--- Gamma/src/main/java/org/beargineers/gamma/Intake.kt
+++ Gamma/src/main/java/org/beargineers/gamma/Intake.kt
@@ -1,13 +1,25 @@
 package org.beargineers.gamma
 
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.beargineers.platform.DoubleNormalDistribution
 import org.beargineers.platform.Frame
 import org.beargineers.platform.Hardware
+import org.beargineers.platform.config
 import org.beargineers.platform.decode.IntakeMode
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
+
+private val ONE_ARTIFACTS_CURRENT_THRESHOLD by config(1500) // Typical current ~1900ma +-60
+private val TWO_ARTIFACTS_CURRENT_THRESHOLD by config(2800) // Typical current ~3300ma +-160
+private val THREE_ARTIFACTS_CURRENT_THRESHOLD by config(4000) // Typical current ~4500ma +-260
 
 class Intake(val bot: GammaRobot): Hardware(bot) {
     private val intake: DcMotor by hardware("intake")
+    private val motorCurrent = DoubleNormalDistribution(10)
+
+    public var artifactsCount = 0
+
     var mode: IntakeMode = IntakeMode.OFF
 
     override fun init() {
@@ -16,6 +28,26 @@ class Intake(val bot: GammaRobot): Hardware(bot) {
 
     override fun loop() {
         Frame.addData("Intake", mode.name)
+        motorCurrent.update((intake as DcMotorEx).getCurrent(CurrentUnit.MILLIAMPS))
+        val (current, currentStd) = motorCurrent.result()
+        Frame.graph("IntakeCurrentMA", current)
+        Frame.graph("IntakeCurrentMASTD", currentStd)
+
+        if (mode == IntakeMode.ON) {
+            artifactsCount = when {
+                current > THREE_ARTIFACTS_CURRENT_THRESHOLD -> 3
+                current > TWO_ARTIFACTS_CURRENT_THRESHOLD -> 2
+                current > ONE_ARTIFACTS_CURRENT_THRESHOLD ->  1
+                else -> 0
+            }
+
+/*
+            if (artifactsCount == 3) {
+                mode = IntakeMode.OFF
+                robot.opMode.gamepad1.rumble(300)
+            }
+*/
+        }
 
         intake.power = when (mode) {
             IntakeMode.ON -> {
