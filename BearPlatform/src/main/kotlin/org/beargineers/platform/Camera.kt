d7200db2a0b43d1325bed0d4911852f6082a7da0
Maxim Shafirov <max@shafirov.com>
2026-04-26 22:13:17 +0200

    Covert LL camera to robot center position in code rather than in LL dashboard. Hopefully it is easier to debug

--- BearPlatform/src/main/kotlin/org/beargineers/platform/Camera.kt
+++ BearPlatform/src/main/kotlin/org/beargineers/platform/Camera.kt
@@ -1,5 +1,12 @@
 package org.beargineers.platform
 
 abstract class Camera(robot: BaseRobot) : Hardware(robot), AbsoluteLocalizer {
+    val CameraPosition_forward by config(0.0)
+    val CameraPosition_up by config(0.0)
+    val CameraPosition_right by config(0.0)
+    val CameraPosition_pitch by config(0.0)
+    val CameraPosition_yaw by config(0.0)
+    val CameraPosition_roll by config(0.0)
+
     // Distance parameters in cm, angles in degrees
 }
