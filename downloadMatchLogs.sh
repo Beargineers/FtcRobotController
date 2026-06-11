fa61692b3d2b98bed971fe17acb8641e2b3692fb
Maxim Shafirov <max@shafirov.com>
2026-06-11 21:57:01 +0200

    Rename *.txt => *.log for the downloaded logs

--- downloadMatchLogs.sh
+++ downloadMatchLogs.sh
@@ -34,6 +34,27 @@ if [ $rc -ne 0 ]; then
   exit $rc
 fi
 
+echo "Renaming log files to .log"
+out=$(find "$extract_dir" -type f -name "*.txt" -exec sh -c '
+  for file do
+    target="${file%.txt}.log"
+    if [ -e "$target" ]; then
+      echo "Refusing to overwrite $target"
+      exit 1
+    fi
+    mv "$file" "$target" || exit $?
+  done
+' sh {} + 2>&1)
+rc=$?
+
+if [ $rc -ne 0 ]; then
+  rm -rf "$extract_dir"
+  echo "Renaming log files failed with error $rc."
+  echo "Output was:"
+  echo "$out"
+  exit $rc
+fi
+
 rm -f "$output_file"
 
 echo "OK"
